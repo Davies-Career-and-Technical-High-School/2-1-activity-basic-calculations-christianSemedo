daca62a503329c878a901470266f3ccbc5fb3505
Christian Semedo <christian.semedo@daviestech.org>
2020-10-08 14:04:06 +0000

    done

---
+++ question1.py
@@ -0,0 +1,3 @@
+x = int(input("enter anumber; "))
+
+print("\n" + str(x + 1) + "\n" + str(x + 2) + "\n" + str(x + 3))
---
+++ question2.py
@@ -0,0 +1,7 @@
+x = int(input("enter a number: "))
+y = int(input("enter number 2: "))
+z = int(input("enter number 3: "))
+
+print(str((x + y)*z))
+
+print(2 ** 2 ** 3 * 3)
